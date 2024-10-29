9df479669ae8493b2ee57c2dc7dd5d3a9876fcb9
Poling <poling980319@gmail.com>
2024-10-29 11:04:45 -0400

    added create and fetch orders controller

--- src/orders/orders.module.ts
+++ src/orders/orders.module.ts
@@ -1,8 +1,24 @@
 import { Module } from '@nestjs/common';
-import { OrdersController } from './orders.controller';
+import { MongooseModule } from '@nestjs/mongoose';
 import { OrdersService } from './orders.service';
+import { OrdersController } from './orders.controller';
+import { User, UserSchema } from '../users/user.schema';
+import { Product, ProductSchema } from '../products/product.schema';
+import { Order, OrderSchema } from 'src/orders/order.schema';
+import {
+  BusinessUser,
+  BusinessUserSchema,
+} from 'src/business-users/business-user.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Order.name, schema: OrderSchema },
+      { name: User.name, schema: UserSchema },
+      { name: Product.name, schema: ProductSchema },
+      { name: BusinessUser.name, schema: BusinessUserSchema },
+    ]),
+  ],
   controllers: [OrdersController],
   providers: [OrdersService],
 })
--- src/orders/orders.service.ts
+++ src/orders/orders.service.ts
@@ -1,4 +1,65 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model, Types } from 'mongoose';
+import { CreateOrderDto } from 'src/dto/order.dto';
+import { User } from 'src/users/user.schema';
+import { Product } from 'src/products/product.schema';
+import { Order } from 'src/orders/order.schema';
+import { BusinessUser } from 'src/business-users/business-user.schema';
 
 @Injectable()
-export class OrdersService {}
+export class OrdersService {
+  constructor(
+    @InjectModel(Order.name) private orderModel: Model<Order>,
+    @InjectModel(User.name) private userModel: Model<User>,
+    @InjectModel(Product.name) private productModel: Model<Product>,
+    @InjectModel(BusinessUser.name)
+    private businessUserModel: Model<BusinessUser>,
+  ) {}
+
+  async createOrder(
+    userId: string,
+    createOrderDto: CreateOrderDto,
+    productId: string,
+  ): Promise<Order> {
+    const user = await this.userModel.findById(userId);
+    if (!user) {
+      throw new NotFoundException('User not found');
+    }
+
+    const product = await this.productModel.findById(productId);
+    if (!product) {
+      throw new NotFoundException('Product not found');
+    }
+
+    const businessUserId = product.businessUser;
+    if (!businessUserId) {
+      throw new NotFoundException('Business user for the product not found');
+    }
+
+    const totalPrice = product.price * createOrderDto.quantity;
+
+    const newOrder = new this.orderModel({
+      ...createOrderDto,
+      user: user._id,
+      product: product._id,
+      businessUser: businessUserId,
+      totalPrice,
+    });
+
+    return newOrder.save();
+  }
+
+  async getAllOrdersByUser(businessUserId: string) {
+    const businessUser = await this.businessUserModel.findById(businessUserId);
+    if (!businessUser) {
+      throw new NotFoundException('Business user not found');
+    }
+
+    return this.orderModel
+      .find({ businessUser: new Types.ObjectId(businessUserId) })
+      .populate('user', 'firstName lastName')
+      .populate('product', 'name price')
+      .exec();
+  }
+}
